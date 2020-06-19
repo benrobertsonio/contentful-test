cc6ba21eb0e6a4ccc5ede58e2fd611b0fb60e084
Ben Robertson <hi@benrobertson.io>
2020-06-19 11:22:44 -0400

    update master.

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,6 +16,7 @@ class RootIndex extends React.Component {
       <Layout location={this.props.location} >
         <div style={{ background: '#fff' }}>
           <Helmet title={siteTitle} />
+          <h1>This is the home page on master.</h1>
           <Hero data={author.node} />
           <div className="wrapper">
             <h2 className="section-headline">Recent articles</h2>
